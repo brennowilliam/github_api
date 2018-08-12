bcf3bf3562a59e36416a61844fe1ea67101acf64
brennowilliam <brennowilliam@gmail.com>
2018-08-12 15:53:51 -0300

    SERVER: Add handle error middleware

--- server/app/src/configMiddleware.js
+++ server/app/src/configMiddleware.js
@@ -3,6 +3,16 @@ import bodyParser from "body-parser"
 const setGlobalMiddleWare = app => {
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
+
+  app.use((req, res, next) => {
+    res.status(404).send('404 - Route not found')
+  })
+
+  app.use((err,req, res, next) => {
+    if (err) {
+      res.status(500).send(error)
+    }
+  })
 }
 
 export default setGlobalMiddleWare
