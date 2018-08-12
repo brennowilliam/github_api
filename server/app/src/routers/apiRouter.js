39571dafbf9c9f540678cc2d37f4365d3eb8a899
brennowilliam <brennowilliam@gmail.com>
2018-08-12 18:25:46 -0300

    SERVER: cleaning up ApiRouter

--- server/app/src/routers/apiRouter.js
+++ server/app/src/routers/apiRouter.js
@@ -1,22 +1,17 @@
 import express from 'express'
-
 // Controllers
 import reposCtrl from "../controllers/reposController"
 import bookmarksCtrl from "../controllers/bookmarksController"
 
-
 const apiRouter = express.Router()
-
 // GET /search/react -> Returns assets that contains react in their name
 apiRouter.get('/search/:term', reposCtrl.fetchAll)
-
 // POST /bookmark/1 -> Returns an specific asset if it exists
 // GET /bookmark -> Returns all bookmarks
 apiRouter.route('/bookmarks')
   .get(bookmarksCtrl.fetchAll)
   .post(bookmarksCtrl.create)
-
 // DELETE /bookmark/:id -> Delete a bookmark
-apiRouter.delete('/bookmarks/:id', bookMarksController.delete)
+apiRouter.delete('/bookmarks/:id', bookmarksCtrl.delete)
 
 export default apiRouter
