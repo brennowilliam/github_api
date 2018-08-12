96c7a3cafb5e7ac339374b42dcbedc49b4276b8b
brennowilliam <brennowilliam@gmail.com>
2018-08-12 16:40:20 -0300

    SERVER: Fixes middleware and add errors handler

--- server/app/src/configMiddleware.js
+++ server/app/src/configMiddleware.js
@@ -3,16 +3,6 @@ import bodyParser from "body-parser"
 const setGlobalMiddleWare = app => {
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
-
-  app.use((req, res, next) => {
-    res.status(404).send('404 - Route not found')
-  })
-
-  app.use((err,req, res, next) => {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
 }
 
 export default setGlobalMiddleWare
--- server/app/src/server.js
+++ server/app/src/server.js
@@ -10,8 +10,16 @@ const app = express()
 setupMiddleware(app)
 setupRouters(app)
 
+
 app.use((req, res, next) => {
   res.status(404).send('404 - Route not found')
+  next()
+})
+
+app.use((err,req, res, next) => {
+  if (err) {
+    res.status(500).send(err)
+  }
 })
 
 export default app
